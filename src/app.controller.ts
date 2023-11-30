cc2aaf75b3258a38cfcec1e971fd699ab9561d2f
Alexandre Junior <alexandrejunior408@gmail.com>
2023-11-30 10:08:34 -0300

    feat: creates controller to receive data of the external backend

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { EventPattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @EventPattern('task-notification')
+  async taskNotification(data: any) {
+    console.log('recebendo mensagem', data);
   }
 }
